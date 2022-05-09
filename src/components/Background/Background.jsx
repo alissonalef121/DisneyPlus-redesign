7f5b0fa9640cabf325eb167cb0413045a5782182
alissonalef121 <alissonalef121@gmail.com>
2022-05-09 20:46:27 -0300

    Deploy

--- src/components/Background/Background.jsx
+++ src/components/Background/Background.jsx
@@ -3,7 +3,7 @@ import "./Background.scss"
 export function Background(){
     return (
         <div className="background_wrapper">
-            <img src="\assets\img\luca-background.png" alt="Current movie background" />
+            <img src="assets\img\luca-background.png" alt="Current movie background" />
         </div>
     )
 }
